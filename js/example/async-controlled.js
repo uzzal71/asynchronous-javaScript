65c9c20a546faf57f17bfecab3a871251a24b922
uzzal71 <uzzalroy.acm@gmail.com>
2023-07-07 00:11:18 +0600

    async call order fixed

--- js/example/async-controlled.js
+++ js/example/async-controlled.js
@@ -1,16 +1,24 @@
-const takeOrder = (customer) => {
+const takeOrder = (customer, callback) => {
     console.log(`take order for ${customer}`);
+    callback(customer);
 }
 
-const processOrder = (customer) => {
+const processOrder = (customer, callback) => {
     console.log(`Processing order for ${customer}`);
 
     setTimeout(() => {
         console.log(`order processed for ${customer}`);
         console.log(`cooking completed`);
+        callback(customer);
     }, 3000);
 };
 
 const completeOrder = (customer) => {
     console.log(`completed order for ${customer}`);
 };
+
+takeOrder("customer 1", (customer) => {
+    processOrder(customer, (customer) => {
+        completeOrder(customer);
+    });
+});
